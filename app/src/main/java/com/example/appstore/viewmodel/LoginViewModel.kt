7f3fd1f586a6b7c5a04b44b66ec0e0ce0a53f07e
Pratheek Tumati <pratheek.t@ezetap.com>
2023-07-06 07:42:35 +0530

    Added State value

--- app/src/main/java/com/example/appstore/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/appstore/viewmodel/LoginViewModel.kt
@@ -6,30 +6,40 @@ import androidx.lifecycle.ViewModel
 import com.example.appstore.data.LoginRequestData
 import com.example.appstore.data.LoginResponseData
 import com.example.appstore.repository.LoginRepository
+import com.example.appstore.service.ResponseListenerState
 import com.example.networkmodule.network.ResponseListener
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class LoginViewModel : ViewModel() {
 
     var loginRepository: LoginRepository? = null
 
     private var loginSuccessMutableLiveData = MutableLiveData<LoginResponseData>()
-    private var loginNetworkFailureMutableLiveData = MutableLiveData<Throwable>()
+    private var loginNetworkFailureMutableLiveData = MutableLiveData<String>()
     private var loginFailureMutableLiveData = MutableLiveData<String>()
+
+    val loginState = MutableStateFlow<ResponseListenerState>(ResponseListenerState.Empty)
 
 
     fun doLogin(loginRequestData: LoginRequestData) {
         loginRepository = LoginRepository.getInstance()
+        loginState.value = ResponseListenerState.Start
         loginRepository!!.doLogin(loginRequestData, object : ResponseListener {
             override fun onSuccess(success: Any?) {
                 loginSuccessMutableLiveData.value = success as LoginResponseData
+                loginState.value = ResponseListenerState.onSuccess(success as LoginResponseData)
             }
 
-            override fun onNetworkFailure(error: Throwable) {
+            override fun onNetworkFailure(error: String) {
                 loginNetworkFailureMutableLiveData.value = error
+                loginState.value = ResponseListenerState.onNetworkError(error)
             }
 
             override fun onFailure(parseError: String?) {
                 loginFailureMutableLiveData.value = parseError
+                if (parseError != null) {
+                    loginState!!.value = ResponseListenerState.onFailure(parseError)
+                }
             }
 
         })
@@ -37,7 +47,7 @@ class LoginViewModel : ViewModel() {
 
     fun successLoginData(): LiveData<LoginResponseData> = loginSuccessMutableLiveData
 
-    fun networkFailureUIData(): LiveData<Throwable> = loginNetworkFailureMutableLiveData
+    fun networkFailureUIData(): LiveData<String> = loginNetworkFailureMutableLiveData
 
     fun errorLoginData(): LiveData<String> = loginFailureMutableLiveData
 }
